ab39eb1df6c2abcd78d3189417dd0a2f386d2891
赖海斌 <12211612@mail.sustech.edu.cn>
2023-02-15 23:07:16 +0800

    2023.2.15

---
+++ src/Accepted/No09.java
@@ -0,0 +1,20 @@
+package Accepted;
+
+public class No09 {
+    public boolean isPalindrome(int x) {
+        String s =x+"";
+        String AntiS = "";
+        for(int i=0;i<s.length();i++){
+            AntiS=AntiS+s.charAt(s.length()-i-1);
+        }
+        boolean y =AntiS.equals(s);
+        return y;
+
+
+    }
+
+    public static void main(String[] args) {
+        No09 s =new No09();
+        s.isPalindrome(1234);
+    }
+}
---
+++ src/CompiledError/No20.java
@@ -0,0 +1,9 @@
+package CompiledError;
+
+public class No20 {
+    public boolean isValid(String s) {
+        char[] t =s.toCharArray();
+        return true;
+
+    }
+}
