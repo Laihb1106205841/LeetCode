d787d310a313fd7ed2971274431d55c4a59a8bef
赖海斌 <12211612@mail.sustech.edu.cn>
2023-02-18 15:45:20 +0800

    2023.2.15

---
+++ src/Accepted/No16.java
@@ -0,0 +1,22 @@
+package Accepted;
+
+public class No16 {
+    public int threeSumClosest(int[] nums, int target) {
+        int l=nums.length;
+        int Closest=100000;
+        int Small =0;
+        for(int i=0;i<l-2;i++){
+            for (int j=i+1;j<l-1;j++){
+                for(int k=j+1;k<l;k++){
+                    int Sum =nums[i]+nums[j]+nums[k];
+                    if(Math.abs(Sum-target)<=Closest){
+                        Small=Sum;
+                        Closest=Math.abs(Sum-target);}
+
+                }
+            }
+        }
+
+        return Small;
+    }
+}
