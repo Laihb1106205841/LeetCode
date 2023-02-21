cf276616e2539569b22ee69786838b1ed194848a
赖海斌 <12211612@mail.sustech.edu.cn>
2023-02-22 00:22:19 +0800

    2023.2.22 今天的打卡！

---
+++ src/Accepted/No88.java
@@ -0,0 +1,19 @@
+package Accepted;
+
+import java.util.Arrays;
+
+public class No88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] F=new int[m+n];
+        for(int i=0;i<m;i++){
+            F[i]=nums1[i];
+        }
+        for(int i=0;i<n;i++){
+            F[m+i]=nums2[i];
+        }
+        Arrays.sort(F);
+        for(int i=0;i<m+n;i++){
+            nums1[i]=F[i];
+        }
+    }
+}
