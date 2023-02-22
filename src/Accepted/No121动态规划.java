b9e2f8e1017b460b1fee16869eb38f329b0be7e8
赖海斌 <12211612@mail.sustech.edu.cn>
2023-02-22 19:35:36 +0800

    2023.2.22 今天的打卡！

--- src/Accepted/No121动态规划.java
+++ src/Accepted/No121动态规划.java
@@ -5,15 +5,14 @@ public class No121动态规划 {
         if(prices.length==1){return 0;}
         int Min =prices[0];
         int LocalMax =prices[1]-prices[0];
-        for(int i=0;i<prices.length;i++){
-            if(prices[i]<Min){Min=prices[i];}
-            else if(prices[i]-Min>=LocalMax){
-                LocalMax =prices[i]-Min;
+        for (int price : prices) {
+            if (price < Min) {
+                Min = price;
+            } else if (price - Min >= LocalMax) {
+                LocalMax = price - Min;
             }
         }
+        return Math.max(LocalMax, 0);
 
-        if(LocalMax<=0){return 0;}
-
-        return LocalMax;
     }
 }
